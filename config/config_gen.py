382f9bb22966cca04b71d24e4f1e40ee30176020
Rin Yokoyama <ryokoyam@utk.edu>
2018-11-15 19:46:09 +0900

    added config_gen.py

---
+++ config/config_gen.py
@@ -0,0 +1,24 @@
+import argparse
+import yaml
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('-i','--input_file')
+    parser.add_argument('-o','--output_file')
+    parser.add_argument('-b','--block_to_replace')
+    parser.add_argument('-k','--key_to_replace')
+    parser.add_argument('-v','--value_to_replace')
+    
+    args = parser.parse_args()
+    
+    input_file = open(args.input_file)
+    yaml_data = yaml.load(input_file)
+    input_file.close()
+    
+    yaml_data[args.block_to_replace][args.key_to_replace] = args.value_to_replace
+    
+    output_file = open(args.output_file,'w')
+    output_file.write(yaml.dump(yaml_data,default_flow_style=False))
+    output_file.close()
+
