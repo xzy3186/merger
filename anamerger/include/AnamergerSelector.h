fd7eee4ebe720cc4f1b7e95260fe22b8709ad49b
Rin Yokoyama <rinyokoyama88@gmail.com>
2019-07-09 16:27:10 -0400

    updates for the new position map

--- anamerger/include/AnamergerSelector.h
+++ anamerger/include/AnamergerSelector.h
@@ -26,44 +26,44 @@
 #include "OutputTreeData.hpp"
 
 class AnamergerSelector : public TSelector {
-public :
+public:
 
-  AnamergerSelector(TTree * =0);
-  virtual ~AnamergerSelector();
+	AnamergerSelector(TTree* = 0);
+	virtual ~AnamergerSelector();
 
-  virtual Int_t   Version() const { return 2; }
-  virtual void    Init(TTree* mergedData);
-  virtual void    Begin(TTree *mergedData);
-  virtual void    SlaveBegin(TTree *mergedData);
-  virtual Bool_t  Notify() { return kTRUE;}
-  virtual Bool_t  Process(Long64_t entry);
-  virtual void    SetOption(const char *option) { fOption = option; }
-  virtual void    SetObject(TObject *obj) { fObject = obj; }
-  virtual void    SetInputList(TList *input) { fInput = input; }
-  virtual TList  *GetOutputList() const { return fOutput; }
-  virtual void    SlaveTerminate(){}
-  virtual void    Terminate();
+	virtual Int_t   Version() const { return 2; }
+	virtual void    Init(TTree* mergedData);
+	virtual void    Begin(TTree* mergedData);
+	virtual void    SlaveBegin(TTree* mergedData);
+	virtual Bool_t  Notify() { return kTRUE; }
+	virtual Bool_t  Process(Long64_t entry);
+	virtual void    SetOption(const char* option) { fOption = option; }
+	virtual void    SetObject(TObject* obj) { fObject = obj; }
+	virtual void    SetInputList(TList* input) { fInput = input; }
+	virtual TList* GetOutputList() const { return fOutput; }
+	virtual void    SlaveTerminate() {}
+	virtual void    Terminate();
 
-  void SetOutputFileName(const std::string &file_name){
-    output_file_name_ = file_name;
-  }
+	void SetOutputFileName(const std::string& file_name) {
+		output_file_name_ = file_name;
+	}
 
 protected:
 
-  TTreeReader tree_reader_;
-  TTreeReaderValue <OutputTreeData<PspmtData,OutputTreeData<PspmtData,TreeData>>> beta_;
-  TTreeReaderValue <std::vector<processor_struct::CLOVERS>> clover_vec_;
-  TTreeReaderValue <std::vector<processor_struct::VANDLES>> vandle_vec_;
-  ULong64_t total_entry_;
+	TTreeReader tree_reader_;
+	TTreeReaderValue <OutputTreeData<PspmtData, OutputTreeData<PspmtData, TreeData>>> beta_;
+	TTreeReaderValue <std::vector<processor_struct::CLOVERS>> clover_vec_;
+	TTreeReaderValue <std::vector<processor_struct::VANDLES>> vandle_vec_;
+	ULong64_t total_entry_;
 
-  // array for histograms
-  TObjArray* fHistArray;
-  // output file
-  TFile * fOutputFile;
-  std::string output_file_name_;
-  TF1* n_correction;
+	// array for histograms
+	TObjArray* fHistArray;
+	// output file
+	TFile* fOutputFile;
+	std::string output_file_name_;
+	TF1* n_correction;
 
-  ClassDef(AnamergerSelector,1)
+	ClassDef(AnamergerSelector, 1)
 };
 
 #endif
